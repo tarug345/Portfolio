883fbecd782f2b2923aecc03295b63fbbf22bf83
tarug345 <atrothunder.laflame345@gmail.com>
2024-09-26 16:19:37 -0300

    pt 3

--- controller/projetos/menu.js
+++ controller/projetos/menu.js
@@ -6,11 +6,27 @@ let div3 = document.getElementById('div3');
 export function trocarMenu(){
     if(menu.style.left === '0vw'){
         menu.style.left = '-100vw';
+
+        div1.style.rotate = ('0deg');
+        div1.style.position = ('relative');
+        div1.style.top = ('0vh');
+
+        div2.style.display = ('block');
+
+        div3.style.rotate = ('-0deg');
+        div3.style.top = ('0vh');
+        div3.style.position = ('relative');
     }else{
         menu.style.left = '0vw';
 
         div1.style.rotate = ('45deg');
         div1.style.position = ('absolute');
         div1.style.top = ('3vh');
+
+        div2.style.display = ('none');
+
+        div3.style.rotate = ('-45deg');
+        div3.style.top = ('3vh');
+        div3.style.position = ('absolute');
     }
 }
