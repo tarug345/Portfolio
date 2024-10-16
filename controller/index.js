39f77a3d4e19245a431dd8419cfa76fcc72d23f7
tarug345 <atrothunder.laflame345@gmail.com>
2024-10-16 06:53:07 -0300

    aas

---
+++ controller/index.js
@@ -0,0 +1,12 @@
+document.getElementById('section-one').addEventListener('click', function() {
+    // Redireciona para o site desejado
+    window.location.href = 'https://www.example.com'; // Altere para o URL que deseja
+});
+document.getElementById('section-two').addEventListener('click', function() {
+    // Redireciona para o site desejado
+    window.location.href = 'https://www.example.com'; // Altere para o URL que deseja
+});
+document.getElementById('section-three').addEventListener('click', function() {
+    // Redireciona para o site desejado
+    window.location.href = 'https://g.co/kgs/EBNhjw3'; // Altere para o URL que deseja
+});
