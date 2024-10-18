66cb595913f0061031c00cec65766d78237d56d7
tarug345 <atrothunder.laflame345@gmail.com>
2024-10-18 17:26:38 -0300

    aaaaaaaaaa

--- controller/index.js
+++ controller/index.js
@@ -1,10 +1,10 @@
 document.getElementById('section-one').addEventListener('click', function() {
     // Redireciona para o site desejado
-    window.location.href = 'https://www.example.com'; // Altere para o URL que deseja
+    window.location.href = 'https://youtu.be/bFPY5pBw4uo?si=EDVjFH2jT8KkZAFW'; // Altere para o URL que deseja
 });
 document.getElementById('section-two').addEventListener('click', function() {
     // Redireciona para o site desejado
-    window.location.href = 'https://www.example.com'; // Altere para o URL que deseja
+    window.location.href = 'https://en.wikipedia.org/wiki/Travis_Scott'; // Altere para o URL que deseja
 });
 document.getElementById('section-three').addEventListener('click', function() {
     // Redireciona para o site desejado
